5a0997ae2cba06dc2c9399cbce2d15d472040e44
Yukai Huang <yukaihuangtw@gmail.com>
2024-05-04 00:19:26 +0800

    feat: simple timelapsed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n", "@vueuse/nuxt"],
   app: {
     head: {
       charset: "utf-8",
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,32 @@
 <template>
-  <div class="w-full h-screen flex flex-col bg-[#ffff77]">
-    <div class="flex justify-end px-2 py-2 absolute top-0 left-0 w-full">
-      <button
-        class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-        @click="toggleLocale"
-      >
-        中/Eng
-      </button>
+	<div>
+    <div class="w-full h-screen flex flex-col bg-[#ffff77]">
+      <div class="flex justify-end px-2 py-2 absolute top-0 left-0 w-full">
+        <button
+          class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+          @click="toggleLocale"
+        >
+          中/Eng
+        </button>
+      </div>
+      <div class="flex-grow flex flex-col justify-center items-center">
+        <h1 class="text-4xl md:text-6xl font-bold text-center text-gray-800 mb-8">
+        	{{ t('home.question') }}
+        </h1>
+        <p class="text-8xl md:text-9xl font-bold text-red-600">
+          {{ t('answer.no') }}
+        </p>
+      </div>
+
+      <div class="flex justify-start px-2 py-2 absolute bottom-0 left-0 w-full">
+      	<p>
+      		{{ t('since', { time: timelapsed }) }}
+      	</p>
+      </div>
     </div>
-    <div class="flex-grow flex flex-col justify-center items-center">
-      <h1 class="text-4xl md:text-6xl font-bold text-center text-gray-800 mb-8">
-      	{{ t('home.question') }}
-      </h1>
-      <p class="text-8xl md:text-9xl font-bold text-red-600">
-        {{ t('answer.no') }}
-      </p>
+
+    <div class="h-10">
+    	hello
     </div>
   </div>
 </template>
@@ -23,9 +35,10 @@
 const { locale, setLocale, t } = useI18n()
 const switchLocalePath = useSwitchLocalePath()
 
-definePageMeta({
-  title: 'pages.title.index'
-})
+// Not used for now
+// definePageMeta({
+//   title: 'pages.title.index'
+// })
 
 const toggleLocale = () => {
 	if (locale.value === 'en') {
@@ -35,4 +48,30 @@ const toggleLocale = () => {
 	}
 }
 
+// 2013/5/3
+const theDay = new Date(2013, 4, 3)
+const timelapsed = ref(getTimelapsed())
+
+function getTimelapsed() {
+  const now = Date.now();
+  const difference = now - theDay.getTime();
+
+  // Calculate the time components
+  const seconds = Math.floor(difference / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+
+  // Calculate the remaining time components
+  const remainingHours = hours % 24;
+  const remainingMinutes = minutes % 60;
+  const remainingSeconds = seconds % 60;
+
+  // Format the time elapsed string
+  return t('timelapsed', { days, remainingHours, remainingMinutes, remainingSeconds })
+}
+
+useIntervalFn(() => {
+  timelapsed.value = getTimelapsed()
+}, 1000)
 </script>
