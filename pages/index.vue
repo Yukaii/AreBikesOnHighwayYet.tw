d0731527536bc5b412a3912d055e32391986da9a
Yukai Huang <yukaihuangtw@gmail.com>
2024-05-03 23:44:10 +0800

    i18n: home

--- pages/index.vue
+++ pages/index.vue
@@ -3,23 +3,35 @@
     <div class="flex justify-end px-2 py-2 absolute top-0 left-0 w-full">
       <button
         class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        @click="toggleLocale"
       >
         中/Eng
       </button>
     </div>
     <div class="flex-grow flex flex-col justify-center items-center">
       <h1 class="text-4xl md:text-6xl font-bold text-center text-gray-800 mb-8">
-        In Taiwan, are motorbikes on the highway yet?
+      	{{ t('home.question') }}
       </h1>
-      <p class="text-8xl md:text-9xl font-bold text-red-600">NO</p>
+      <p class="text-8xl md:text-9xl font-bold text-red-600">
+        {{ t('answer.no') }}
+      </p>
     </div>
   </div>
 </template>
 
 <script setup>
-const { locale, locales, t } = useI18n()
+const { locale, setLocale, t } = useI18n()
 
 definePageMeta({
   title: 'pages.title.index'
 })
+
+const toggleLocale = () => {
+	if (locale.value === 'en') {
+		setLocale('zh-TW')
+	} else {
+		setLocale('en')
+	}
+}
+
 </script>
